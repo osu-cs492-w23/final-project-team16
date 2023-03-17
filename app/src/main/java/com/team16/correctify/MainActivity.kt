716ec7d812ba6fc6ad3cb5870e12c17a14106ba8
Dylan Mitchel Karambut <62045657+dylanmitchel09@users.noreply.github.com>
2023-03-17 13:46:16 -0700

    Added Save Prototype Button & Setting Up Android Permission

--- app/src/main/java/com/team16/correctify/MainActivity.kt
+++ app/src/main/java/com/team16/correctify/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, ".txt Output File Has Been Saved!", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
