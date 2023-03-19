581f53fedf47ad8cc1d30790679834fe607c8efb
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2023-03-19 14:01:21 -0700

    add clear text button

--- app/src/main/java/com/team16/correctify/ui/SecondFragment.kt
+++ app/src/main/java/com/team16/correctify/ui/SecondFragment.kt
@@ -1,10 +1,10 @@
 package com.team16.correctify.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.team16.correctify.R
 import com.team16.correctify.databinding.FragmentSecondBinding
@@ -23,11 +23,10 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
