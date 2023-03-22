a790b367c843abd41bd74a973b6b3816b9f910ce
dylanmitchel09 <Dylanmitchel09@outlook.com>
2023-03-21 23:00:57 -0700

    Added Save .txt File Function

--- app/src/main/java/com/team16/correctify/ui/MainActivity.kt
+++ app/src/main/java/com/team16/correctify/ui/MainActivity.kt
@@ -3,15 +3,20 @@ package com.team16.correctify.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.team16.correctify.BuildConfig
 import com.team16.correctify.R
 import com.team16.correctify.databinding.ActivityMainBinding
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+
 
 /* To use your own OpenAI API key here, create a file called `gradle.properties` in your
 * GRADLE_USER_HOME directory (this will usually be `$HOME/.gradle/` in MacOS/Linux and
@@ -38,10 +43,39 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, getString(R.string.text_saved), Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            // TODO: save to storage
+        binding.fab.setOnClickListener {
+            // https://gist.github.com/codinginflow/6c13bd0d08416115798f17d45b5d8056
+
+            // Saves the output of the output as "output.txt"
+            // Data stored on: "/data/data/com.team16.correctify/files/output.txt"
+            val FILE_NAME = "output.txt"
+
+            val mEditText = findViewById<TextView>(R.id.result_text)
+
+            val text: String = mEditText.getText().toString()
+            var fos: FileOutputStream? = null
+
+            try {
+                fos = openFileOutput(FILE_NAME, MODE_PRIVATE)
+                fos.write(text.toByteArray())
+                // mEditText.getText().clear()
+                Toast.makeText(
+                    this, "Saved to $filesDir/$FILE_NAME",
+                    Toast.LENGTH_LONG
+                ).show()
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
         }
 
         supportActionBar?.title = getString(R.string.app_name)
